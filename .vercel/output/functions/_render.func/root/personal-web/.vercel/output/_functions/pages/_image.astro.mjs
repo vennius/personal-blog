e0dd813361a9be8125f9f607fccd34aabf5357e1
vennius <89004475+vennius@users.noreply.github.com>
2024-07-13 00:50:50 +0700

    Delete .vercel/output directory

--- .vercel/output/functions/_render.func/root/personal-web/.vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../renderers.mjs';
-
-const page = () => import('../chunks/generic_Bt6ngp0F.mjs');
-
-export { page };
--- .vercel/output/functions/_render.func/root/personal-web/.vercel/output/_functions/pages/blog/test.astro.mjs
+++
@@ -1,5 +0,0 @@
-export { renderers } from '../../renderers.mjs';
-
-const page = () => import('../../chunks/test_STkRpfom.mjs');
-
-export { page };
